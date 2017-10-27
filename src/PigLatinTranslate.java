daf2fe61a518b53e7835d051f7bc6c610376e24f
Alex Cyr <alexcyr@Alex-Cyrs-MacBook-Pro.local>
2017-10-27 08:54:55 -0400

    refactor code to methods

--- src/PigLatinTranslate.java
+++ src/PigLatinTranslate.java
@@ -20,35 +20,13 @@ public class PigLatinTranslate {
 
 		while (continueRun == 'y') {
 
-			StringBuilder sb = new StringBuilder();
 			
 			System.out.println();
 			System.out.print("Enter a line to be translated: ");
 			System.out.println();
-			userInput = scnr.nextLine().toLowerCase();
-			String[] words = userInput.split(" ");
+			userInput = scnr.nextLine();
 			
-			for(int i = 0; i < words.length; i ++) {
-				String newString = "";
-				char firstLetter = words[i].charAt(0);
-				switch(firstLetter) {
-				case 'a':
-				case 'e':
-				case 'i':
-				case 'o':
-				case 'u':
-					newString = words[i].concat("way ");
-					break;
-				default:
-					newString = words[i].substring(1).concat(String.valueOf(firstLetter)).concat("ay ");
-					break;
-				}
-				sb.append(newString);
-				
-
-			}
-			
-			System.out.println(sb);
+			System.out.println(translateString(userInput));
 
 			System.out.println();
 			System.out.print("Translate another line? (y/n) ");
@@ -57,11 +35,47 @@ public class PigLatinTranslate {
 			
 			continueRun = scnr.nextLine().toLowerCase().charAt(0);
 		}
-		System.out.println();
 		System.out.println("Program terminated.");
 
 		scnr.close();
 
+	}
+	
+	public static String translateString(String userInput) {
+		
+		StringBuilder sb = new StringBuilder();
+		String[] words = userInput.split(" ");
+		
+		for(int i = 0; i < words.length; i ++) {
+			String newString = "";
+			char firstLetter = words[i].charAt(0);
+			boolean upperCase = Character.isUpperCase(words[i].charAt(0));
+			if (words[i].indexOf("@") >= 0) {
+				newString = words[i].concat(" ");
+			}
+			else {
+			switch(firstLetter) {
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+				newString = words[i].concat("way ");
+				break;
+			default:
+				newString = words[i].substring(1).concat(String.valueOf(firstLetter)).concat("ay ").toLowerCase();
+				if (upperCase){
+					newString = newString.substring(0, 1).toUpperCase() + newString.substring(1);
+				}
+				break;
+			}
+			}
+			sb.append(newString);
+			
+
+		}
+		
+		return String.valueOf(sb);
 	}
 
 }
